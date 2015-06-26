8a361bce92730a2935173fe039e794f3f99a0198
Murilo Adriano Vasconcelos <muriloufg@gmail.com>
2015-06-25 23:04:03 -0700

    Adds a condition for ending the game
    Whichever team reaches 5 points first wins the game.

--- game/dota_addons/salvebandeirinha/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/salvebandeirinha/scripts/vscripts/addon_game_mode.lua
@@ -1,35 +1,35 @@
 -- Generated from template
 if GM == nil then
-	GM = class({})
+  GM = class({})
 end
 
 if score == nil then
-	score = class({})
-	score.Bad = 0
-	score.Good = 0
+  score = class({})
+  score.Bad = 0
+  score.Good = 0
 end
 
 function Precache( context )
   PrecacheItemByNameSync("item_capture_good_flag", context)
   PrecacheItemByNameSync("item_capture_bad_flag", context)
   PrecacheUnitByNameSync("npc_dota_hero_dragon_knight", context)
-	--[[
-		Precache things we know we'll use.  Possible file types include (but not limited to):
-			PrecacheResource( "model", "*.vmdl", context )
-			PrecacheResource( "soundfile", "*.vsndevts", context )
-			PrecacheResource( "particle", "*.vpcf", context )
-			PrecacheResource( "particle_folder", "particles/folder", context )
-	]]
+  --[[
+    Precache things we know we'll use.  Possible file types include (but not limited to):
+      PrecacheResource( "model", "*.vmdl", context )
+      PrecacheResource( "soundfile", "*.vsndevts", context )
+      PrecacheResource( "particle", "*.vpcf", context )
+      PrecacheResource( "particle_folder", "particles/folder", context )
+  ]]
 end
 
 -- Create the game mode when we activate
 function Activate()
-	GameRules.AddonTemplate = GM()
-	GameRules.AddonTemplate:InitGameMode()
+  GameRules.AddonTemplate = GM()
+  GameRules.AddonTemplate:InitGameMode()
 end
 
 function GM:InitGameMode()
-	print("[Salve Bandeirinha] Iniciando.")
+  print("[Salve Bandeirinha] Iniciando.")
 
   -- Set GameMode parameters
   GameMode = GameRules:GetGameModeEntity()
@@ -43,37 +43,37 @@ function GM:InitGameMode()
   -- não recomenda itens
   GameMode:SetRecommendedItemsDisabled(true)
   
-	-- Setup rules
-	GameRules:SetHeroRespawnEnabled(true)
-	GameRules:SetUseUniversalShopMode(true)
-	GameRules:SetSameHeroSelectionEnabled(true)
-	GameRules:SetHeroSelectionTime(15.0)
-	GameRules:SetPreGameTime(0.0)
-	GameRules:SetPostGameTime(30.0)
-	--GameRules:MakeTeamLose(DOTA_TEAM_GOODGUYS || DOTA_TEAM_BADGUYS) -- faz um time perder. acho que vamos precisar.
-	
-	print("[Salve Bandeirinha] Gamemode rules are set.")
+  -- Setup rules
+  GameRules:SetHeroRespawnEnabled(true)
+  GameRules:SetUseUniversalShopMode(true)
+  GameRules:SetSameHeroSelectionEnabled(true)
+  GameRules:SetHeroSelectionTime(15.0)
+  GameRules:SetPreGameTime(0.0)
+  GameRules:SetPostGameTime(30.0)
+  --GameRules:MakeTeamLose(DOTA_TEAM_GOODGUYS || DOTA_TEAM_BADGUYS) -- faz um time perder. acho que vamos precisar.
+  
+  print("[Salve Bandeirinha] Gamemode rules are set.")
 
   --
-	ListenToGameEvent('npc_spawned', Dynamic_Wrap(GM, 'OnNPCSpawned'), self)
+  ListenToGameEvent('npc_spawned', Dynamic_Wrap(GM, 'OnNPCSpawned'), self)
   ListenToGameEvent('entity_hurt', Dynamic_Wrap(GM, 'OnEntityHurt'), self)
 
   --Coloca as bandeiras no mapa
-	spawnGoodFlag()
-	spawnBadFlag()
+  spawnGoodFlag()
+  spawnBadFlag()
 
-	-- Register Think
-	GameRules:GetGameModeEntity():SetThink("OnThink", self, "GlobalThink", 2)
+  -- Register Think
+  GameRules:GetGameModeEntity():SetThink("OnThink", self, "GlobalThink", 2)
 end
 
 -- Evaluate the state of the game
 function GM:OnThink()
-	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
-		--print("[Salve Bandeirinha] Template addon script is running.")
-	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
-		return nil
-	end
-	return 1
+  if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+    --print("[Salve Bandeirinha] Template addon script is running.")
+  elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
+    return nil
+  end
+  return 1
 end
 
 --[[
@@ -94,21 +94,21 @@ end
 -- Se é atacado por inimigo alteramos o time para customx para ele poder ser atacado por aliado e congelamos.
 -- Se é atacado por aliado retornamos para o time original e descongelamos.
 function GM:OnEntityHurt(keys)
-	local bateu = EntIndexToHScript(keys.entindex_attacker)
-	local apanhou = EntIndexToHScript(keys.entindex_killed)
+  local bateu = EntIndexToHScript(keys.entindex_attacker)
+  local apanhou = EntIndexToHScript(keys.entindex_killed)
 
-	print("time bateu: "..bateu:GetTeamNumber())
-	print("time apanhou: "..apanhou:GetTeamNumber())
-	
+  print("time bateu: "..bateu:GetTeamNumber())
+  print("time apanhou: "..apanhou:GetTeamNumber())
+  
   --DOTA_UNIT_CAP_MOVE_NONE
   --DOTA_UNIT_CAP_MOVE_GROUND
-	apanhou:SetMoveCapability(DOTA_UNIT_CAP_MOVE_NONE)
+  apanhou:SetMoveCapability(DOTA_UNIT_CAP_MOVE_NONE)
   --DOTA_UNIT_CAP_NO_ATTACK
   --DOTA_UNIT_CAP_MELEE_ATTACK
-	apanhou:SetAttackCapability(DOTA_UNIT_CAP_NO_ATTACK)
-	if apanhou:GetTeamNumber() == DOTA_TEAM_GOODGUYS then
-	  apanhou:SetTeam(DOTA_TEAM_CUSTOM_1)
-	  print("tem que dropar item")
+  apanhou:SetAttackCapability(DOTA_UNIT_CAP_NO_ATTACK)
+  if apanhou:GetTeamNumber() == DOTA_TEAM_GOODGUYS then
+    apanhou:SetTeam(DOTA_TEAM_CUSTOM_1)
+    print("tem que dropar item")
     for i = 0,5 do
       print("i:"..i)
       local item = apanhou:GetItemInSlot(i)
@@ -121,9 +121,9 @@ function GM:OnEntityHurt(keys)
         end
       end
     end
-	elseif apanhou:GetTeamNumber() == DOTA_TEAM_BADGUYS then
-	  apanhou:SetTeam(DOTA_TEAM_CUSTOM_2)
-	  print("tem que dropar item")
+  elseif apanhou:GetTeamNumber() == DOTA_TEAM_BADGUYS then
+    apanhou:SetTeam(DOTA_TEAM_CUSTOM_2)
+    print("tem que dropar item")
     for i = 0,5 do
       print("i:"..i)
       local item = apanhou:GetItemInSlot(i)
@@ -139,25 +139,38 @@ function GM:OnEntityHurt(keys)
   elseif apanhou:GetTeamNumber() == DOTA_TEAM_CUSTOM_1 then
     if bateu:GetTeamNumber() == DOTA_TEAM_GOODGUYS then
       apanhou:SetMoveCapability(DOTA_UNIT_CAP_MOVE_GROUND)
-	    apanhou:SetAttackCapability(DOTA_UNIT_CAP_MELEE_ATTACK)
-	    apanhou:SetTeam(DOTA_TEAM_GOODGUYS)
+      apanhou:SetAttackCapability(DOTA_UNIT_CAP_MELEE_ATTACK)
+      apanhou:SetTeam(DOTA_TEAM_GOODGUYS)
     end
   elseif apanhou:GetTeamNumber() == DOTA_TEAM_CUSTOM_2 then
     if bateu:GetTeamNumber() == DOTA_TEAM_BADGUYS then
       apanhou:SetMoveCapability(DOTA_UNIT_CAP_MOVE_GROUND)
-	    apanhou:SetAttackCapability(DOTA_UNIT_CAP_MELEE_ATTACK)
-	    apanhou:SetTeam(DOTA_TEAM_BADGUYS)
+      apanhou:SetAttackCapability(DOTA_UNIT_CAP_MELEE_ATTACK)
+      apanhou:SetTeam(DOTA_TEAM_BADGUYS)
     end
-	end
+  end
 end
 
 function GM:updateScore(scoreGood, scoreBad)
   print("entrou update score")
   print(scoreGood)
   print(scoreBad)
+
   local GameMode = GameRules:GetGameModeEntity()
   GameMode:SetTopBarTeamValue(DOTA_TEAM_GOODGUYS, scoreGood)
   GameMode:SetTopBarTeamValue(DOTA_TEAM_BADGUYS, scoreBad)
+
+  -- If any team reaches this score the game ends and that team is considered
+  -- winner.
+  local SCORE_TO_WIN = 5
+  if scoreGood == SCORE_TO_WIN then
+    print("Team GOOD GUYS victory!")
+    GameRules:SetGameWinner(DOTA_TEAM_GOODGUYS)
+  end
+  if scoreBad == SCORE_TO_WIN then
+    print("Team BAD GUYS victory!")
+    GameRules:SetGameWinner(DOTA_TEAM_BADGUYS)
+  end
 end
 
 function spawnGoodFlag()
@@ -173,7 +186,3 @@ function spawnBadFlag()
   CreateItemOnPositionSync(Vector(1920, -256, 128), flag)
   print("saiu spawnBadFlag")
 end
-
-
-
-
